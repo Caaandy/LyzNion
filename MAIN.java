fdef7d4f4d84f7f4e58e9e4764b5d768c94fb89a
lilli <lillianapilkenrodt@gamil.com>
2019-05-21 14:15:23 +0200

    .

--- MAIN.java
+++ MAIN.java
@@ -7,68 +7,52 @@ public class MAIN extends JFrame {
     private Nion n;
     private BANK bank;
     private STORY blubb;
-    private JTextField name;
+    private JTextField name1;
+    private JTextField name2;
     private JButton button1;
     private JButton button2;
-    private JPanel panel1;
-    private JPanel panel2;
+    private JPanel panel;
     private JLabel oben;
     private JLabel anzeige;
-    
+
     public MAIN(){
-        super("Fenster");
-        start();
-        setSize(300,300);
-        setLocation(500,500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        AnmeldenRegistrieren();
+       AnmeldenRegistrieren();
     }
-    
-         public void addButtonListener(JButton b)
+
+    public void addButtonListener(JButton b)
     {
-        b.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent ae)
+            b.addActionListener(new ActionListener()
             {
-                eingabe(ae.getActionCommand());
-            }
-        });
+                public void actionPerformed(ActionEvent ae)
+                {
+                    eingabe(ae.getActionCommand());
+                }
+            });
     }
+
     private void eingabe(String a)
     {
         anzeige.setText(a);
-        Anmelden();
-        name = new JTextField("Dein Name");
     }
-    
+
     public void AnmeldenRegistrieren() {
-        setLayout(new GridLayout(3,1));
-        panel1 = new JPanel(new GridLayout(1,1));
-        panel2 = new JPanel(new GridLayout(1,1));
+        setSize(300,300);
+        setLocation(500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(1,1));
+        panel = new JPanel(new GridLayout(2,1));
         button1 = new JButton("Anmelden");
         button2 = new JButton("Registrieren");
-        panel1.add(button1);
-        panel2.add(button2);
+        name1 = new JTextField("name");
+        name2 = new JTextField("name");
+        panel.add(button1);
+        panel.add(name1);
+        panel.add(button2);
+        panel.add(name2);
         addButtonListener(button1);
         addButtonListener(button2);
-        oben = new JLabel("aaaaaaaaaaaaaaaaaa");
         anzeige = new JLabel();
-        oben.setHorizontalAlignment(JLabel.CENTER);
-        getContentPane().add(anzeige);
-        getContentPane().add(panel1);
-        getContentPane().add(panel2);
+        getContentPane().add(panel);
         setVisible(true);
     } 
-    public void Anmelden()
-    {
-        
-    }
-    
-    public void start()
-    {
-        MAIN g = new MAIN();   
-    }
-    
-    public void auswählen() {
-    }
 }
