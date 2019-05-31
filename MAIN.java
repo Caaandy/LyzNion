04df2fba799fa3341bd15d67f81bc21c1a4bb1aa
Phil <candy@byom.de>
2019-05-31 10:50:56 +0200

    j

--- MAIN.java
+++ MAIN.java
@@ -2,57 +2,77 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class MAIN extends JFrame {
+public class MAIN implements ActionListener {
     private Lyz l; 
     private Nion n;
     private BANK bank;
     private STORY blubb;
+    private JFrame fenster;
+    private JPanel panel;
     private JTextField name1;
     private JTextField name2;
     private JButton button1;
     private JButton button2;
-    private JPanel panel;
-    private JLabel oben;
-    private JLabel anzeige;
+    private String na;
 
     public MAIN(){
-       AnmeldenRegistrieren();
+       //elemente + buttonsListener
+       button1 = new JButton("Anmelden");
+       button1.addActionListener(this);
+       button2 = new JButton("Registrieren");
+       button2.addActionListener(this);
+       name1 = new JTextField("name");
+       name2 = new JTextField("name");
+       
+       //panel = new JPanel(new GridLayout(2,1));
+       panel = new JPanel(new GridLayout(2,1));
+       panel.add(button1);
+       panel.add(name1);
+       panel.add(button2);
+       panel.add(name2);
+       
+       //fenster
+       fenster = new JFrame("Fenster");
+       fenster.setSize(300,300);
+       fenster.setLocation(500,500);
+       fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       fenster.add(panel);
+       fenster.setVisible(true);
     }
-
-    public void addButtonListener(JButton b)
-    {
-            b.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent ae)
-                {
-                    eingabe(ae.getActionCommand());
+    
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button1)
+        {
+            try {
+                if(bank.userSuchen(name1.getText())== false) {
+                    System.out.println("yay");
+                    charakterWahl();
                 }
-            });
+                else {
+                    System.out.println("nope");
+                }
+            }
+            catch(Exception ex)
+            {
+                name1.setText("User nicht vorhanden!");
+            }
+        }
+        if(e.getSource() == button2)
+        {
+            try {
+                if(bank.userSuchen(name2.getText()) == false) {
+                    charakterWahl();
+                }
+            }
+            catch(Exception ex)
+            {
+                name2.setText("Name schon vorhanden!");
+            }
+        }
     }
-
-    private void eingabe(String a)
-    {
-        anzeige.setText(a);
+    
+    private void charakterWahl() {
+        System.out.println("Yay! :D");
+        blubb = l;
     }
-
-    public void AnmeldenRegistrieren() {
-        setSize(300,300);
-        setLocation(500,500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(1,1));
-        panel = new JPanel(new GridLayout(2,1));
-        button1 = new JButton("Anmelden");
-        button2 = new JButton("Registrieren");
-        name1 = new JTextField("name");
-        name2 = new JTextField("name");
-        panel.add(button1);
-        panel.add(name1);
-        panel.add(button2);
-        panel.add(name2);
-        addButtonListener(button1);
-        addButtonListener(button2);
-        anzeige = new JLabel();
-        getContentPane().add(panel);
-        setVisible(true);
-    } 
 }
